30c59cf8a830c2ffe254708f1ac4a220739746ab
Vitoria-m <vitoriazzz@outlook.com>
2022-08-26 11:00:46 -0300

    landing page complete

--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -1,10 +1,15 @@
 import './category.scss'
 
 import { Link } from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 import { popularItensData } from '../../services/popularItensData'
-import FeaturedProducts from '../../components/featured/featured'
+import { featuredData } from '../../services/featuredProducts'
+
+import line from '../../assets/line.png'
+import icon from '../../assets/icon.png'
+import arrow_1 from '../../assets/arrow_1.png'
+import arrow_2 from '../../assets/arrow_2.png'
 
 function CategoryLandingPage() {  
     const colors = ['white', '#43c0cf', '#3d402f', '#4aaf00', '#315bbd', '#ec6115', '#dc041d', '#efead7']
@@ -14,6 +19,18 @@ function CategoryLandingPage() {
     const slice = popularItensData.slice(0, next)
     const loadMore = () => {
         setNext(next + 4)
+    }
+
+    const carousel = useRef(null)
+
+    const handleLeftClick = (e) => {
+        e.preventDefault()
+        carousel.current.scrollLeft -= carousel.current.offsetWidth
+    }
+    
+    const handleRightClick = (e) => {
+        e.preventDefault()
+        carousel.current.scrollLeft += carousel.current.offsetWidth
     }
 
     return (
@@ -103,7 +120,33 @@ function CategoryLandingPage() {
             
             <button className='loadMoreButton' onClick={() => loadMore()}>LOAD MORE</button>
 
-            {/* <FeaturedProducts/> */}
+            <div className='featuredContainerCategory'>
+                <h1>Featured Products</h1>
+                <h4>Unde omnis iste natus error sit voluptatem</h4>
+
+                <div className='featuredIcons'>
+                    <button onClick={handleLeftClick}><img src={arrow_1} alt='Scroll Left'/></button>
+                    <img src={line} alt='White Line' className='line'/>
+                    <img src={icon} alt='Three Vertical Bars'/>
+                    <img src={line} alt='White Line' className='line'/>
+                    <button onClick={handleRightClick}><img src={arrow_2} alt='Scroll Right'/></button>
+                </div>
+                
+                <div className='featuredCards' ref={carousel}>
+                    {featuredData.map(data => (
+                        <div className='card' key={data.id}>
+                                <div className='cardImage'>
+                                    <img src={data.path} alt=''/>
+                                </div>
+
+                                <div className='cardText'>
+                                    <h3>{data.name}</h3>
+                                    <h4>{data.category}</h4>
+                                </div>     
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
